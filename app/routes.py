92ff9e11c70fd69c8091e20f86c4553222ad21f0
vp-nocode <166547991+vp-nocode@users.noreply.github.com>
2024-09-11 00:58:19 +0500

    mod flask app 2

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,61 @@
-# from flask import render_template, request, redirect, url_for
-from app import app, db, User
+from flask import render_template, request, redirect, url_for
+from flask_login import login_user, logout_user, current_user, login_required
+from app import app, db, models, forms
+from app.models import User
+from app.forms import RegistrationForm, LoginForm
+import bcrypt
 
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+        user = User(username=form.username.data, email=form.email.data, password=hashed_password)
+        db.session.add(user)
+        db.session.commit()
+        flash('Вы успешно зарегистрировались', 'success')
+        return redirect(url_for('login'))
+
+    return render_template('register.html', form=form)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user and bcrypt.check_password_hash(user.password, form.password.data):
+            login_user(user, remember=form.remember.data)
+            return redirect(url_for('home'))
+        else:
+            flash('Введены неверные данные', 'danger')
+
+    return render_template('login.html', form=form)
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('home'))
+
+
+@app.route('/account')
+@login_required
+def account():
+    return render_template('account.html')
+
+
+'''
 # The function creates an application context, which is needed to work with the database
 with app.app_context():
     # Creating a table defined in the User class
