088efe003cd9ca8e5b73265414c18fbbc538424e
vp-nocode <166547991+vp-nocode@users.noreply.github.com>
2024-09-10 20:25:14 +0500

    mod flask app

--- app/__init__.py
+++ app/__init__.py
@@ -1,24 +1,21 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+
 
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = 'your_secret_secret_key'
 # connect to db
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///example.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 # Disabling the alarm about changes to objects inside the database
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 # Creating an object to work with a database
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+# The module will redirect the user to the route we specify (to authorization)
+login_manager.login_view = 'login'
 
 from app import routes
-
-# In brackets we indicate the model in order to create a database later
-class User(db.Model):
-    # In brackets we describe the table fields
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True, nullable=False)
-
-    def __repr__(self):
-        # This method determines how the model object will look as a string
-        return f'<User {self.username}>'
---
+++ app/models.py
@@ -0,0 +1,19 @@
+from app import db, login_manager
+from flask_login import UserMixin # This class allows you to work with the user
+
+@login_manager.user_loader
+def load_user(user_id):
+    # This line will send a query to the DB to search for a specific user by his ID
+    return User.query.get(int(user_id))
+
+# In brackets we indicate the model in order to create a database later
+class User(db.Model, UserMixin):
+    # In brackets we describe the table fields
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(30), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(60), nullable=False)
+
+    def __repr__(self):
+        # This method determines how the model object will look as a string
+        return f'<User {self.username}, email: {self.emai}>'
